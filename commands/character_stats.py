830ffe3a235054dde0428f4f62616371047a11c1
reynaldo <reynaldo280602@gmail.com>
2025-04-20 22:55:20 +1000

    f

--- commands/character_stats.py
+++ commands/character_stats.py
@@ -187,8 +187,8 @@ class UnitInfo(commands.Cog):
                 ephemeral=True
             )
             return
-        total_tracked_matches = await self.get_total_tracked_matches(debut_date)
-
+        total_tracked_matches = await self.get_total_tracked_matches(debut_date.isoformat())
+        
         embed = Embed(
             title=f"Pick/Ban Data for {row['name']}",
             color=0xB197FC
