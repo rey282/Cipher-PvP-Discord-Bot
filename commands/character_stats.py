c0da12250633b90ae5c8468818f64d394a93b156
reynaldo <reynaldo280602@gmail.com>
2025-04-21 17:37:56 +1000

    fx

--- commands/character_stats.py
+++ commands/character_stats.py
@@ -53,6 +53,7 @@ class StatsButton(discord.ui.Button):
         if interaction.user.id != view.user_id:
             await interaction.response.send_message("A-ack, it seems like you can’t interact with this menu... P-please, use the appropriate command yourself to unlock the threads of fate!", ephemeral=True)
             return
+        await interaction.response.defer()
         new_data = await view.cog.fetch_stats_data(mode=self.custom_id)
         new_view = StatsView(view.cog, self.custom_id, new_data, user_id=view.user_id)
         await interaction.response.edit_message(embed=new_view.get_embed(), view=new_view)
